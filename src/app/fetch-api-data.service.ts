64bed65e6ad506ecc2d629391fe2988208a2f71e
lac1990 <155014030+lac1990@users.noreply.github.com>
2025-02-18 11:12:21 -0600

    6.3

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -8,7 +8,7 @@ const apiUrl = 'movie-api-main-3.onrender.com/';
 @Injectable({
   providedIn: 'root'
 })
-export class UserRegistrationService {
+export class FetchApiDataService{
   // Inject the HttpClient module to the constructor params
   // This will provide HttpClient to the entire class, making it available via this.http
   constructor(private http: HttpClient) {
